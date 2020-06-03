8a376d1f98ad4862315034f439726e22a6f1cfac
rsabha <rsabha@tuta.io>
2020-06-03 21:20:47 +0200

    5.6 - saving data in the controller

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,4 +1,18 @@
 class ArticlesController < ApplicationController
     def new
     end
+
+    def create
+      @article = Article.new(article_params)
+      
+      @article.save
+      redirect_to @article
+      
+      #render plain: params[:article].inspect # display params and debug
+    end
+
+    private
+      def article_params
+        params.require(:article).permit(:title, :text)
+      end
 end
